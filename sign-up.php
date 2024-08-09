5b15caa953adc4ffa334bccde5daabac455a0c1b
1grey1 <k0rol11@yandex.ru>
2024-08-09 23:58:31 +0300

    pegistration validations

--- sign-up.php
+++ sign-up.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 require_once './core/init.php';
 
+$errors = [];
+
+$fields = [
+    'email',
+    'password',
+    'name',
+    'contact_info',
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    foreach ($fields as $key) {
+        if (isset($_POST[$key]) && trim($_POST[$key]) === '') {
+            $errors[$key] = 'error massage';
+        }
+    }
+}
+
 /** @var $categories */
 $categoryList = includeTemplate('_partials/category-list.php', [
     'categories' => $categories,
@@ -11,16 +28,17 @@ $categoryList = includeTemplate('_partials/category-list.php', [
 
 $pageContent = includeTemplate('sign-up.php', [
     'categoryList' => $categoryList,
+    'errors'       => $errors,
 ]);
 
 /** @var $authStatus */
 /** @var $user */
 $layoutContent = includeTemplate('layout/main.php', [
-    'authStatus'  => $authStatus,
+    'authStatus'   => $authStatus,
     'categoryList' => $categoryList,
-    'pageContent' => $pageContent,
-    'title'       => 'sign-up',
-    'user'        => $user,
+    'pageContent'  => $pageContent,
+    'title'        => 'sign-up',
+    'user'         => $user,
 ]);
 
 print $layoutContent;
--- templates/sign-up.php
+++ templates/sign-up.php
@@ -1,33 +1,78 @@
 <?php
 
 /** @var string $categoryList */
+/** @var array $errors */
 
 ?>
 <?= $categoryList ?>
 
 <!-- form--invalid -->
-<form class="form container form--invalid" action="" method="post" autocomplete="off">
+<form
+    class="form container form--invalid"
+    action="sign-up.php"
+    method="post"
+    autocomplete="off"
+    enctype="application/x-www-form-urlencoded"
+>
     <h2>Регистрация нового аккаунта</h2>
-    <div class="form__item"> <!-- form__item--invalid -->
+    <div class="form__item<?= isset($errors['email']) ? ' form__item--invalid' : ''?>">
         <label for="email">E-mail <sup>*</sup></label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail">
-        <span class="form__error">Введите e-mail</span>
+        <input
+            id="email"
+            type="text"
+            name="email"
+            value="<?= $_POST['email'] ?? '' ?>"
+            placeholder="Введите e-mail"
+        >
+
+        <?php if (isset($errors['email'])): ?>
+            <span class="form__error"><?= $errors['email'] ?></span>
+        <?php endif; ?>
     </div>
-    <div class="form__item">
+
+    <div class="form__item<?= isset($errors['password']) ? ' form__item--invalid' : ''?>">
         <label for="password">Пароль <sup>*</sup></label>
-        <input id="password" type="password" name="password" placeholder="Введите пароль">
-        <span class="form__error">Введите пароль</span>
+        <input
+            id="password"
+            type="password"
+            name="password"
+            value="<?= $_POST['password'] ?? '' ?>"
+            placeholder="Введите пароль"
+        >
+
+        <?php if (isset($errors['password'])): ?>
+            <span class="form__error"><?= $errors['password'] ?></span>
+        <?php endif; ?>
     </div>
-    <div class="form__item">
+
+    <div class="form__item<?= isset($errors['name']) ? ' form__item--invalid' : ''?>">
         <label for="name">Имя <sup>*</sup></label>
-        <input id="name" type="text" name="name" placeholder="Введите имя">
-        <span class="form__error">Введите имя</span>
+        <input
+                id="name"
+                type="text"
+                name="name"
+                value="<?= $_POST['name'] ?? '' ?>"
+                placeholder="Введите имя"
+        >
+
+        <?php if (isset($errors['name'])): ?>
+            <span class="form__error"><?= $errors['name'] ?></span>
+        <?php endif; ?>
     </div>
-    <div class="form__item">
+
+    <div class="form__item<?= isset($errors['contact_info']) ? ' form__item--invalid' : ''?>">
         <label for="message">Контактные данные <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите как с вами связаться"></textarea>
-        <span class="form__error">Напишите как с вами связаться</span>
+        <textarea
+                id="message"
+                name="contact_info"
+                placeholder="Напишите как с вами связаться"
+        ></textarea>
+
+        <?php if (isset($errors['contact_info'])): ?>
+            <span class="form__error"><?= $errors['contact_info'] ?></span>
+        <?php endif; ?>
     </div>
+
     <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
     <button type="submit" class="button">Зарегистрироваться</button>
     <a class="text-link" href="#">Уже есть аккаунт</a>
