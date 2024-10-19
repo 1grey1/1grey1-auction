e90b5bcfd502f7ac2bc9b8dcde2326a556b19c48
1grey1 <k0rol11@yandex.ru>
2024-10-19 10:34:08 +0300

    fix-3.0

--- templates/layout/main.php
+++ templates/layout/main.php
@@ -17,6 +17,7 @@
     <link href="./resources/css/fix.css" rel="stylesheet">
 </head>
 <body>
+
     <div class="page-wrapper">
         <?= includeTemplate('layout/_main-header.php', [
             'user' => $user,
